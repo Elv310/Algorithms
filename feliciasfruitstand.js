7f9d3f6cdf8c6d87a367908b3e534200b62a762e
Elv310 <Elvisa310@gmail.com>
2021-07-12 20:06:09 -0400

    First version of find largest of four fcc

---
+++ feliciasfruitstand.js
@@ -0,0 +1,45 @@
+
+// let fruits = [["apples", .59], ["bananas", .50], ["oranges", .75], ["avocados", 1.45], ["lemons", .56], ["pears", 1.25]]
+
+
+
+// for(let i = 0; i < fruits.length; i++){
+//     console.log("The",fruits[i][0],"are", fruits[i][1])
+// }
+
+// Problem 1 ^^
+let fruits = [["apples", .59], ["bananas", .50], ["oranges", .75], ["avocados", 1.45], ["lemons", .56], ["pears", 1.25]]
+
+
+// for(let i = 0; i < fruits.length; i++){
+//     // console.log(fruits[i][0] + "\n" + fruits[i][1])
+//     for(let j = 0; j < fruits[i].length; j++){
+//         console.log(fruits[i][j])
+//     }
+// }
+
+
+//Problem 2 ^^
+
+
+for(let i = 0; i < fruits.length; i++){
+    fruits[i][2] = 0.40
+  
+    
+}
+console.log(fruits)
+
+//Problem 3 ^^
+
+for(let i = 0; i < fruits.length; i++){
+    
+}
+
+
+
+// if(x < 10 && x > 5 ||x == 1){
+//     console.log("hello")
+  
+// } else {
+//     console.log("bye")
+// }

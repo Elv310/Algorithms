08643d7990b51505011e5eb06b47c2bf43dc360a
Elv310 <Elvisa310@gmail.com>
2021-07-22 17:28:49 -0400

    "largest number"

---
+++ largestnumber.js
@@ -0,0 +1,15 @@
+function largestOfFour(arr) {
+    let arrLargest = [];
+    for ( let i = 0; i < arr.length; i++){
+        let largestNum = arr[i][0]
+        for (let j = 0; j < arr[i].length; j++){
+            if (arr[i][j] > largestNum){
+                largestNum = arr[i][j];
+            }
+        }
+        arrLargest[i] = largestNum
+    }
+    return arrLargest;
+  }
+  
+  console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));

8006082f9fad3653c09fe7f7094b09176bc611c9
Elv310 <Elvisa310@gmail.com>
2021-07-26 16:55:51 -0400

    "Road trip"

---
+++ cdroadtrip.js
@@ -0,0 +1,38 @@
+let riders = [ ["Arnell"], ["Carlos", "Keith", "Sergio"], ["Junior", "Loubna", "Margie"], ["Travis", "Matt", "Gabriel"], ["Elvis", "Jackie", "Samier"] ]
+// Output each riders name on a new line, and what row of the bus they are in (Arnell is in row 1)
+
+// Arnell 1
+// carlos 2
+// keith 2
+// sergio 2
+// junior 3
+
+for(i = 0; i < riders.length; i++){
+
+ let namesArray = riders[i]
+    for( let i = 0; i < namesArray.length; i++){
+        console.log(namesArray[i]);
+    }
+//     console.log(namesArray.length)
+}    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // console.log(riders[0],"is in row 1") ,console.log(riders[1][0],"is in row 2") , console.log(riders[1][1],"is in row 2"), console.log(riders[1] [2], "is in row 2"), console.log(riders[2] [0], "is in row 3"), console.log(riders[2] [1],"is in row 3"), console.log(riders[2] [2], "is in row 3"), console.log(riders [3] [0],"is in row 4"), console.log(riders[3] [1],"is in row 4"), console.log(riders[3] [2], "is in row 4"), console.log(riders[4] [0],"is in row 5"), console.log(riders[4] [1],"is in row 5"), console.log(riders[4] [2],"is in row 5")

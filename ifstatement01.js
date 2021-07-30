c6686c6a53b88bdce7572a493dd6d6879b392427
Elv310 <Elvisa310@gmail.com>
2021-07-29 23:30:54 -0400

    "ifstatement01"

---
+++ ifstatement01.js
@@ -0,0 +1,12 @@
+let firstName = "Sam";
+let longName 
+
+// console.log("Hello",name)
+// console.log(name.length);
+
+if (firstName.length > 3) {
+    console.log(firstName) 
+    longName = firstName
+}
+
+console.log(longName)

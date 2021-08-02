26c2b8922b5410c3e8de933c553014d9fe30c31c
Elv310 <Elvisa310@gmail.com>
2021-08-01 22:38:01 -0400

    "BA002"

---
+++ BA002.js
@@ -0,0 +1,49 @@
+// convertToF(0) should return a number
+
+// convertToF(-30) should return a value of -22
+
+// convertToF(-10) should return a value of 14
+
+// convertToF(0) should return a value of 32
+
+// convertToF(20) should return a value of 68
+
+// convertToF(30) should return a value of 86
+
+
+
+function convertToF(celsius) {
+    let fahrenheit = celsius*9/5+32
+    
+    console.log(fahrenheit , celsius)
+    
+    return fahrenheit;
+  }
+  
+  convertToF(30);
+  convertToF(-10);
+
+
+
+
+
+
+
+
+
+// function reverseString(str) {
+//     return str;
+//   }
+  
+//   reverseString("hello");
+
+
+
+
+
+
+//   function factorialize(num) {
+//     return num;
+//   }
+  
+//   factorialize(5);
